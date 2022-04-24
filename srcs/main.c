a0018cfb60076cb42ce880b63fbc9dda43d4dc53
s-ike <shunt_i@yahoo.co.jp>
2022-04-24 17:39:23 +0900

    update main process

--- srcs/main.c
+++ srcs/main.c
@@ -8,74 +8,91 @@
 #include "def_pipex.h"
 #include "def_error.h"
 
-int
-	main(int ac, char **av)
+static void
+	child_process(char **argv, char **environ, int fds[PIPE_NUM])
 {
-	extern char	**environ;
+	int	fd;
 
-	if (ac != ARG_NUM)
+	(void)environ;
+	close(fds[PIPE_R]);
+	if ((fd = open(argv[ARG_FILE1], O_RDONLY, 0666)) < 0)
 	{
-		ft_puterror_and_exit_failure(ft_puterror, ERR_INVAL);
+		ft_puterror_and_exit_failure(ft_putperror, argv[ARG_FILE1]);
 	}
 
+	dup2(fds[PIPE_W], STDOUT_FILENO);
+	dup2(fd, STDIN_FILENO);
+	close(fds[PIPE_W]);
+	close(fd);
+
+	char	*av[2];
+	av[0] = argv[ARG_CMD1];
+	av[1] = NULL;
+	execvp(argv[ARG_CMD1], av);
+	ft_puterror_and_exit_failure(ft_putperror, argv[ARG_CMD1]);
+}
+
+static void
+	parent_process(char **argv, char **environ, int fds[PIPE_NUM])
+{
+	(void)environ;
+	close(fds[PIPE_W]);
+
+	dup2(fds[PIPE_R], STDIN_FILENO);
+	close(fds[PIPE_R]);
+
+	int	fd;
+	if ((fd = open(argv[ARG_FILE2], O_WRONLY | O_CREAT | O_TRUNC, 0666)) < 0)
+	{
+		ft_puterror_and_exit_failure(ft_putperror, argv[ARG_FILE2]);
+	}
+
+	dup2(fd, STDOUT_FILENO);
+	close(fd);
+
+	char	*av[2];
+	av[0] = argv[ARG_CMD2];
+	av[1] = NULL;
+
+	execvp(argv[ARG_CMD2], av);
+	ft_puterror_and_exit_failure(ft_putperror, argv[ARG_CMD2]);
+}
+
+static void
+	main_process(char **argv, char **environ)
+{
 	int	fds[PIPE_NUM];
+	pid_t	pid;
 
 	if (pipe(fds) < 0)
 	{
 		ft_puterror_and_exit_failure(ft_putperror, "pipe");
 	}
-
-	pid_t	pid = fork();
+	pid = fork();
 	if (pid < 0)
 	{
 		ft_puterror_and_exit_failure(ft_putperror, "fork");
 	}
 	else if (pid == 0)
 	{
-		close(fds[PIPE_R]);
-
-		int	fd;
-		if ((fd = open(av[ARG_FILE1], O_RDONLY, 0666)) < 0)
-		{
-			ft_puterror_and_exit_failure(ft_putperror, av[ARG_FILE1]);
-		}
-
-		dup2(fds[PIPE_W], STDOUT_FILENO);
-		dup2(fd, STDIN_FILENO);
-		close(fds[PIPE_W]);
-		close(fd);
-
-		char	*argv[2];
-		argv[0] = av[ARG_CMD1];
-		argv[1] = NULL;
-		execvp(av[ARG_CMD1], argv);
-		ft_puterror_and_exit_failure(ft_putperror, av[ARG_CMD1]);
+		child_process(argv, environ, fds);
 	}
 	if (wait(NULL) != pid)
 	{
 		ft_puterror_and_exit_failure(ft_putperror, "wait");
 	}
+	parent_process(argv, environ, fds);
+}
+
+int
+	main(int argc, char **argv)
+{
+	extern char	**environ;
+
+	if (argc != ARG_NUM)
 	{
-		close(fds[PIPE_W]);
-
-		dup2(fds[PIPE_R], STDIN_FILENO);
-		close(fds[PIPE_R]);
-
-		int	fd;
-		if ((fd = open(av[ARG_FILE2], O_WRONLY | O_CREAT | O_TRUNC, 0666)) < 0)
-		{
-			ft_puterror_and_exit_failure(ft_putperror, av[ARG_FILE2]);
-		}
-
-		dup2(fd, STDOUT_FILENO);
-		close(fd);
-
-		char	*argv[2];
-		argv[0] = av[ARG_CMD2];
-		argv[1] = NULL;
-
-		execvp(av[ARG_CMD2], argv);
-		ft_puterror_and_exit_failure(ft_putperror, av[ARG_CMD2]);
+		ft_puterror_and_exit_failure(ft_puterror, ERR_INVAL);
 	}
+	main_process(argv, environ);
 	return (EXIT_FAILURE);
 }
