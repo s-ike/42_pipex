075d9af8fbc6a40932d4707c1e9ce40977ff250a
s-ike <shunt_i@yahoo.co.jp>
2022-05-01 22:51:14 +0900

    update #2 execution with path

--- srcs/ft_execute.c
+++ srcs/ft_execute.c
@@ -6,7 +6,7 @@
 /*   By: sikeda <sikeda@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/29 19:02:03 by sikeda            #+#    #+#             */
-/*   Updated: 2022/04/30 23:16:03 by sikeda           ###   ########.fr       */
+/*   Updated: 2022/05/01 22:41:36 by sikeda           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,9 @@ void
 
 	cmd = ft_split(cmd_str, ' ');
 	cmd_path = NULL;
-	if (cmd)
+	if (cmd && ft_strchr(cmd[0], '/'))
+		cmd_path = ft_strdup(cmd[0]);
+	else if (cmd)
 		cmd_path = ft_get_cmd_path(cmd[0], environ);
 	if (!cmd_path)
 	{
